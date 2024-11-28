2eda90badd1107294cae4d63fd6eec0b482b3f9b
rickysaarloos <9027376@student.zadkine.nl>
2024-11-28 13:26:30 +0100

    opdracht2

---
+++ week2/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+<?php include 'navigatie.php'; ?>   
+ <h1>about</h1>
+ <?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ week2/navigatie.php
@@ -0,0 +1,9 @@
+<nav>
+<ul>
+
+<li><a href="home.php">Home</a></li>
+<li><a href="about">About</a></li>
+<li><a href="contact.php">Contact</a></li>
+    
+</ul>
+</nav>
