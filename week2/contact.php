9553bef86d85c7b50f05c03210156010ae3f733f
rickysaarloos <9027376@student.zadkine.nl>
2024-11-28 13:43:40 +0100

    opdracht3

--- week2/contact.php
+++ week2/contact.php
@@ -8,6 +8,29 @@
 <body>
 <?php include 'navigatie.php'; ?>   
  <h1>contact</h1>
+
+ <form action="#" method="POST"></form>
+<div>
+<label for="naam">Naam</label>
+<input type="text" id="naam" name="naam" required>
+</div>
+<br>
+<div>
+<label for="E-mail">E-mail</label>
+<input type="text" id="E-mail" name="E-mail" required>
+</div>
+<br>
+<div>
+<label for="Bericht">Bericht</label>
+<input type="text" id="Bericht" name="Bericht" required>
+</div>
+
+
+
+
+
+
+
  <?php include 'footer.php'; ?>
 </body>
 </html>
